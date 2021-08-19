0c2c8c20b2fbf3c312198ca6c76fe078706547ff
Peter Donovan <peter@darkfusion.io>
2021-08-18 21:21:29 -0400

    Added actix-web framework and ''main' function Added the `actix-web` and `actix-http` dependencies. Since the beta release version of `actix-web` is being used, we also added `actix-http` explicitly. This helps ensure that our dependencies are compatible.
    The 'scripts' directory was added to the '.gitignore' file. This folder
    contains some useful scripts for local development. However, it does not
    include any functionality that necessitates checking it into VCS.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Starting service. Available at: http://127.0.0.1:8000");
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
+
+pub async fn index() -> impl Responder {
+    HttpResponse::Ok()
 }
