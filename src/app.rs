1a9edd4b04201542e94f5ef06ada505b4fc9c004
Peter Donovan <peter@darkfusion.io>
2021-09-09 21:59:45 -0400

    Add basic telemetry to the application
    Requests are now observable and records are created/kept for later
    inspection. This is accomplished via the addition of "Logging"
    middleware; courtesy of the `tracing` crate and its associated crates.

--- src/app.rs
+++ src/app.rs
@@ -3,17 +3,19 @@
 
 use std::net::TcpListener;
 
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpServer};
+use actix_web::{dev::Server, middleware, web, App, HttpServer};
+use env_logger::Env;
 use sqlx::PgPool;
 
 use crate::handlers::{health, subscribe};
 
 pub fn start(listener: TcpListener, pool: PgPool) -> Result<Server, std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let pool = web::Data::new(pool);
     let server = HttpServer::new(move || {
         App::new()
             .app_data(pool.clone())
+            .wrap(middleware::Logger::default())
             .route("/health", web::get().to(health))
             .route("/subscribe", web::post().to(subscribe))
     })
